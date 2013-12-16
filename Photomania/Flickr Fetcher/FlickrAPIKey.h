752a7742d4f436fdb8af58aa9844f6d6370e9d11
David Wood <dbwood3@gmail.com>
2013-12-15 23:00:21 -0500

    Initial Commit

---
+++ Photomania/Flickr Fetcher/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2013.
+//  Copyright 2013 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"afaf4fbb91193d72af4dd5b4b82238ff"
